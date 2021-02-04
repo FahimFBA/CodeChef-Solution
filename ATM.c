dca4a06aac71cbb57e78c1858f59b3ac3a18ecbc
FahimFBA <fahimbinamin@gmail.com>
2021-02-05 03:53:03 +0600

    ATM solved

---
+++ ATM.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main() 
+{
+    int transaction;
+    float balance ;
+     scanf("%d %f" , &transaction, &balance); 
+     
+
+    if ((transaction % 5 == 0) && (balance  >= (transaction + 0.5)))
+    {
+        printf("%.2f\n" , (balance - (transaction + 0.5)));
+    }
+
+    else
+    printf("%.2f\n" , balance);
+
+    return 0;
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# CodeChef-Solution
+# CodeChef Solution
+
+My CodeChef profile: [Click Here!](https://www.codechef.com/users/fahimfba)
+
+This repo contains all the source codes of mine which I used to solve the problems from CodeChef.
