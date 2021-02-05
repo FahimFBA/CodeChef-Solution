894728b053d53e15203a1ffbf297d31773479024
FahimFBA <fahimbinamin@gmail.com>
2021-02-05 21:20:53 +0600

    Before Calculation - Maximise Function

---
+++ Maximise_Function.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, i , k, n, value;
+    cin >> t;
+    for (i = 0; i < t; i++)
+    {
+        cin >> n;
+        for (k = 0; k < n; k++)
+        {
+            cin >> value;
+        }
+    }
+
+    return 0;
+}
