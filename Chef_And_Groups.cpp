5d21945b823af9620673f93ee1b4208a1669fa65
FahimFBA <fahimbinamin@gmail.com>
2021-03-07 02:56:37 +0600

    haven't solved yet Chef-and-groups

---
+++ Chef_And_Groups.cpp
@@ -0,0 +1,15 @@
+// it's still in on-going section
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    int i;
+    for (i = 0; i < t ; i++)
+    {
+
+    }
+}
