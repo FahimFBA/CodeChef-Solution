d9ae00f935f2e3e9d83e28c0be737c55a3295613
FahimFBA <fahimbinamin@gmail.com>
2021-10-18 00:56:42 +0600

    solved

--- Chef_And_Groups.cpp
+++ Chef_And_Groups.cpp
@@ -1,15 +1,26 @@
-// it's still in on-going section
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int t;
+    int t, i, count = 0;
     cin >> t;
-    int i;
-    for (i = 0; i < t ; i++)
+    string array;
+    while (t--)
     {
-
+        cin >> array;
+        count = 0;
+        for (i = 0; i < array.length(); i++)
+        {
+            if (array[i] == '0' && array[i + 1] == '1')
+            {
+                count++;
+            }
+        }
+        if (array[0] == '1')
+        {
+            count++;
+        }
+        cout << count << endl;
     }
 }
